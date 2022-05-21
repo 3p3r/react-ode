e80febbc1a2fa412cfc2f64f27bee2b461e10974
Sepehr Laal <5657848+3p3r@users.noreply.github.com>
2022-05-20 19:22:20 -0700

    used projen to scaffold a new project for the ode component

---
+++ .projenrc.js
@@ -0,0 +1,32 @@
+const { javascript, web, github } = require("projen");
+
+const projectName = 'react-ode';
+
+const project = new javascript.NodeProject({
+  name: projectName,
+  description: "Online Development Environment in React",
+  packageManager: javascript.NodePackageManager.NPM,
+  keywords: ['react', 'monaco-editor', 'material-ui', 'ode'],
+  repository: `git+https://github.com/3p3r/${projectName}.git`,
+  bugsUrl: `https://github.com/3p3r/${projectName}/issue`,
+  defaultReleaseBranch: "main",
+  autoMerge: false,
+  dependabot: true,
+  dependabotOptions: {
+    scheduleInterval: github.DependabotScheduleInterval.WEEKLY,
+    ignore: [
+      { dependencyName: 'react' },
+      { dependencyName: 'react-dom' },
+      { dependencyName: 'react-scripts' },
+      { dependencyName: 'web-vitals' },
+      { dependencyName: 'jest*' },
+      { dependencyName: '@testing-library/*' },
+    ]
+  }
+});
+
+const component = new web.ReactComponent(project, {
+  typescript: false,
+});
+
+project.synth();
