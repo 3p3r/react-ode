d325a05a0f26b3df1126e86ef6129e338502417a
Sepehr Laal <5657848+3p3r@users.noreply.github.com>
2022-05-29 00:12:37 -0700

    put some comments for the wiring that goes on here

--- src/components/workspace/Console.js
+++ src/components/workspace/Console.js
@@ -30,24 +30,31 @@ export default class Console extends React.Component {
   handleConsoleRef = (el) => {
     if (!el) return this.setState({ shell: null, terminal: null });
     const terminal = new Terminal({ cursorBlink: true, scrollback: 0 });
+    // XtermJSShell uses eventemitter api, here we do a conversion
     terminal.on = (name, ...args) => {
       terminal[`on${_.capitalize(name)}`](...args);
     };
+    // XtermJSShell is older than our xterm and needs some patches
     const shell = new XtermJSShell(terminal);
-    const run = shell.run.bind(shell);
-    // todo: cleanup?
+    // redirect cash-money's virtual console to xterm (todo: cleanup?)
     logger.on('log', (data) => shell.printLine(`${data}`));
-    const read = shell.echo.read.bind(shell.echo);
+    // we hook into where XtermJSShell reads lines and save the last one
     let lastLine = '';
+    const read = shell.echo.read.bind(shell.echo);
     shell.echo.read = async (...args) => {
       const line = await read(...args);
       lastLine = line;
       return line;
     };
+    // we hook into XtermJSShell and pass stuff it does not recognize to cash-money
+    const run = shell.run.bind(shell);
     shell.run = async (command, args, flags) => {
       if (!shell.commands.has(command)) {
+        // this is our "busybox" environment
         await cash(lastLine);
       } else {
+        // this is everything registered with XtermJSShell's api
+        // documented in node_modules/xterm-js-shell/README.md
         return await run(command, args, flags);
       }
     };
