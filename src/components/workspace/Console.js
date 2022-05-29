bf140a69a5215b29b5de67fcd500ea7d0d0b08eb
Sepehr Laal <5657848+3p3r@users.noreply.github.com>
2022-05-29 00:05:56 -0700

    integrated with cash money and another middleware to offer a repl over memfs. elite.
    + memfs is brought in by cash-money and no longer a first class dependency

--- src/components/workspace/Console.js
+++ src/components/workspace/Console.js
@@ -1,9 +1,12 @@
+import _ from 'lodash';
 import React from 'react';
 import { Terminal } from 'xterm';
 import { FitAddon } from 'xterm-addon-fit';
 import { WebglAddon } from 'xterm-addon-webgl';
 import { WebLinksAddon } from 'xterm-addon-web-links';
 import ReactResizeDetector from 'react-resize-detector';
+import XtermJSShell from 'xterm-js-shell';
+import { cash, console as logger } from 'react-ode-cash-money';
 import { css } from '@emotion/react';
 
 import 'xterm/css/xterm.css';
@@ -20,12 +23,35 @@ const styles = css`
 
 export default class Console extends React.Component {
   state = {
+    shell: null,
     terminal: null,
   };
 
   handleConsoleRef = (el) => {
-    if (!el) return this.setState({ terminal: null });
+    if (!el) return this.setState({ shell: null, terminal: null });
     const terminal = new Terminal({ cursorBlink: true, scrollback: 0 });
+    terminal.on = (name, ...args) => {
+      terminal[`on${_.capitalize(name)}`](...args);
+    };
+    const shell = new XtermJSShell(terminal);
+    const run = shell.run.bind(shell);
+    // todo: cleanup?
+    logger.on('log', (data) => shell.printLine(`${data}`));
+    const read = shell.echo.read.bind(shell.echo);
+    let lastLine = '';
+    shell.echo.read = async (...args) => {
+      const line = await read(...args);
+      lastLine = line;
+      return line;
+    };
+    shell.run = async (command, args, flags) => {
+      if (!shell.commands.has(command)) {
+        await cash(lastLine);
+      } else {
+        return await run(command, args, flags);
+      }
+    };
+    shell.repl();
     terminal.open(el);
     terminal.loadAddon(new WebLinksAddon());
     const fitAddon = new FitAddon();
@@ -38,7 +64,7 @@ export default class Console extends React.Component {
     terminal.write('react-ode $ ');
     fitAddon.activate(terminal);
     terminal.refit = () => fitAddon.fit();
-    this.setState({ terminal });
+    this.setState({ shell, terminal });
     this.refit();
   };
 
