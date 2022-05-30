9c89912a47515dffb42f427a9a7b357d4fdb924f
Sepehr Laal <5657848+3p3r@users.noreply.github.com>
2022-05-30 13:06:54 -0700

    basic wiring of Explorer pane to memfs implemented

--- src/components/workspace/Explorer.js
+++ src/components/workspace/Explorer.js
@@ -3,6 +3,8 @@ import Box from '@mui/material/Box';
 import TreeView from '@mui/lab/TreeView';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import directoryTree from '../utils/directory-tree';
+import { fs } from 'react-ode-cash-money';
 import TreeItem from '@mui/lab/TreeItem';
 import { css } from '@emotion/react';
 
@@ -11,22 +13,40 @@ const styles = css`
 `;
 
 export default class Explorer extends React.Component {
+  componentDidMount() {
+    this.createInitialTree();
+  }
+
+  createInitialTree = () => {
+    if (fs.existsSync('/app')) fs.rmdirSync('/app', { recursive: true, force: true });
+    fs.mkdirSync('/app');
+    fs.mkdirSync('/app/assets');
+    fs.writeFileSync('/app/assets/sample.js', '', 'utf8');
+    fs.writeFileSync('/app/assets/sample.py', '', 'utf8');
+    fs.writeFileSync('/app/assets/sample.css', '', 'utf8');
+    fs.writeFileSync('/app/sample.html', '', 'utf8');
+  };
+
+  buildFileTree = () => {
+    const _addJsx = (item, path) =>
+      (item.jsx = (children) => (
+        <TreeItem key={path} nodeId={path} label={item.name}>
+          {children}
+        </TreeItem>
+      ));
+    const tree = directoryTree('/app', {}, _addJsx, _addJsx);
+    const mapper = (node) => (node ? node.jsx(node.children && node.children.map(mapper)) : null);
+    return (
+      <TreeView defaultCollapseIcon={<ExpandMoreIcon />} defaultExpandIcon={<ChevronRightIcon />}>
+        {mapper(tree)}
+      </TreeView>
+    );
+  };
+
   render() {
     return (
       <Box css={styles} sx={{ bgcolor: 'background.default', color: 'text.primary' }}>
-        <TreeView
-          aria-label="file system navigator"
-          defaultCollapseIcon={<ExpandMoreIcon />}
-          defaultExpandIcon={<ChevronRightIcon />}
-        >
-          <TreeItem nodeId="1" label="public">
-            <TreeItem nodeId="3" label="file.css" />
-          </TreeItem>
-          <TreeItem nodeId="2" label="src">
-            <TreeItem nodeId="4" label="file.js" />
-            <TreeItem nodeId="5" label="file.py" />
-          </TreeItem>
-        </TreeView>
+        {this.buildFileTree()}
       </Box>
     );
   }
